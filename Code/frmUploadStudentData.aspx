759d56aca164e0ad39bbf896f2bb302e8df6cd81
AntiSaby <antisaby@gmail.com>
2017-05-03 14:06:47 +0530

    Added UploadStudentData record cell form

---
+++ Code/frmUploadStudentData.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="frmUploadStudentData.aspx.cs" Inherits="frmUploadStudentData" %>
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+     <style>
+        td { padding: 10px 30px 10px 30px; }
+     </Style>
+     <telerik:RadTabStrip ID="RadTabStrip1" runat="server" SelectedIndex="0" MultiPageID="RadMultiPage1">
+        <Tabs>
+            <telerik:RadTab runat="server" Text="Upload Certificates" Font-Bold="true" Selected="True"></telerik:RadTab>
+            <telerik:RadTab runat="server" Text="Search Certificates" Font-Bold="true"></telerik:RadTab>
+        </Tabs>
+    </telerik:RadTabStrip>
+     <telerik:RadMultiPage ID="RadMultiPage1" runat="server" SelectedIndex="0">
+        <telerik:RadPageView ID="RadPageView1" runat="server">
+            <h4>Student Documents Upload</h4>
+         <table style="margin-top:30px">
+             <tr>
+                 <td>Registration No.</td>
+                 <td><asp:TextBox ID="USDRegNotxt" runat="server"></asp:TextBox></td>
+             </tr>
+             <tr>
+                 <td>File Category</td>
+                 <td><telerik:RadComboBox ID="USDCertTypeCombo" runat="server"  Height="200" Width="330" EmptyMessage="Choose File Category" >
+                     <Items>
+                        <telerik:RadComboBoxItem runat="server" Text="Student Admission File" Value="A"/>
+                        <telerik:RadComboBoxItem runat="server" Text="Record Cell Certificates(pass out students)" value="R"/>
+                        <telerik:RadComboBoxItem runat="server" Text="Event Certificates" Value="E"/>
+                     </Items>
+                  </telerik:RadComboBox></td>
+             </tr>
+             <tr>
+                 <td>Select</td>
+                 <td><asp:FileUpload ID="USDFileUpload" runat="server" AllowMultiple="true"/></td>
+             </tr>
+             <tr>
+                 <td colspan="2"><telerik:RadButton ID="USCUploadBtn" runat="server" Text="Upload" OnClick="USCUploadBtn_Click"></telerik:RadButton></td>
+             </tr>
+         </table>
+        </telerik:RadPageView>
+        <telerik:RadPageView ID="RadPageView2" runat="server">
+             <h4>Student Documents Search</h4>
+            <table id="table1" style="margin-top:30px">
+             <tr>
+                 <td>Registration No.</td>
+                 <td><asp:TextBox ID="USDRegNotxt2" runat="server"></asp:TextBox></td>
+             </tr>
+             <tr>
+                 <td>File Category</td>
+                 <td><telerik:RadComboBox ID="USDCertTypeCombo2" runat="server"  Height="200" Width="330" EmptyMessage="Choose File Category" >
+                     <Items>
+                        <telerik:RadComboBoxItem runat="server" Text="Student Admission File" Value="A"/>
+                        <telerik:RadComboBoxItem runat="server" Text="Record Cell Certificates(pass out students)" value="R"/>
+                        <telerik:RadComboBoxItem runat="server" Text="Event Certificates" Value="E"/>
+                     </Items>
+                  </telerik:RadComboBox></td>
+             </tr>
+             <tr>
+                 <td colspan="2">
+                     <telerik:RadButton ID="Search" runat="server" Text="Search" OnClick="Search_Click"></telerik:RadButton></td>
+             </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Table ID="USDFilesTbl" runat="server"> 
+                        <asp:TableHeaderRow><asp:TableHeaderCell>File</asp:TableHeaderCell><asp:TableHeaderCell>Download</asp:TableHeaderCell></asp:TableHeaderRow>
+                    </asp:Table>  
+                </td>
+            </tr>
+            </table>
+        </telerik:RadPageView>
+     </telerik:RadMultiPage>
+</asp:Content>
+
---
+++ Code/frmUploadStudentData.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using Telerik.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using FtpClient;
+using System.Collections;
+
+public partial class frmUploadStudentData : System.Web.UI.Page
+{
+    private static string regno;
+    private static String ft;
+    private static int refreshMode = 0;
+    private static string folderOnFTPServer = "test";
+    private String connectionString = ConfigurationManager.ConnectionStrings["NewUmsConnectionString"].ConnectionString;
+    private static ArrayList links = new ArrayList();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (links.Count > 0)
+        {
+            int i = 0;
+            foreach (string item in links)
+            {
+                Button btn = new Button();
+                btn.ID = ++i + "Nigga";
+                btn.Text = "Download";
+                btn.Click += new EventHandler(downloadFile);
+                TableRow row = new TableRow();
+                TableCell cellName = new TableCell();
+                cellName.Text = "Nigga";
+                row.Cells.Add(cellName);
+                TableCell cellButton = new TableCell();
+                cellButton.Controls.Add(btn);
+                row.Cells.Add(cellButton);
+                USDFilesTbl.Rows.Add(row);
+            }
+        }
+    }
+    protected void USCUploadBtn_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            regno = USDRegNotxt.Text;
+            ft = USDCertTypeCombo.SelectedValue;
+            string fileType = null;
+            if (ft == "A") { fileType = "AdmissionDocument"; } else if (ft == "R") { fileType = "AcademicCertificate"; } else if (ft == "E") { fileType = "EventCertificate"; }
+            int i = 1;
+            foreach (HttpPostedFile postedFile in USDFileUpload.PostedFiles)
+            {
+
+                string ext = Path.GetExtension(postedFile.FileName).ToLower(); 
+
+                string fileName = regno + "_" + fileType + "_" + i + ext;
+
+                FtpService ftpClient = new FtpService();
+                FtpService.FtpCredentials credentials = FtpUserPassword.GetUMSFtpCredentials();
+
+                byte[] imageData = null;
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    using (BinaryReader binaryReader = new BinaryReader(postedFile.InputStream))
+                    {
+                        imageData = binaryReader.ReadBytes(postedFile.ContentLength);
+                    }
+                }
+
+                string fullPath = credentials.Url + folderOnFTPServer + "/" + fileName;
+
+                string result = ftpClient.UploadFile(folderOnFTPServer, fileName, imageData, credentials);
+                
+                if (result.Trim().StartsWith("226") || result.Trim().Contains("complete") || result.Trim().Contains("Success"))
+                {
+                    showPopup("Uploaded!");
+                    Debug.WriteLine("Uploaded!");
+                }
+                else if (result.Trim() == "File Already Exists")
+                {
+                    showPopup("A certificate file already exists with the same name. Please change the name of the image file and try again.");
+                    return;
+                }
+                else
+                {
+                    Debug.WriteLine(result);
+                    showPopup("Something went wrong. Please try again.");
+                    return;
+                }
+                i += 1;
+            }
+        }
+        catch (Exception ex)
+        {
+            showPopup("Something went wrong. Please try again.");
+            Debug.WriteLine(ex.Message);
+        }
+    }
+
+    protected void Search_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            regno = USDRegNotxt2.Text;
+            ft = USDCertTypeCombo2.SelectedValue;
+            string fileType = null;
+            if (ft == "A") { fileType = "AdmissionDocument"; } else if (ft == "R") { fileType = "AcademicCertificate"; } else if (ft == "E") { fileType = "EventCertificate"; }
+
+            ArrayList allFiles = new ArrayList();
+            links.Clear();
+
+            string fileName = regno + "_" + fileType;
+
+            FtpService ftpClient = new FtpService();
+            FtpService.FtpCredentials credentials = FtpUserPassword.GetUMSFtpCredentials();
+            allFiles.AddRange(ftpClient.ViewFile(folderOnFTPServer, credentials));
+            foreach (string item in allFiles)
+            {
+                if(item.Contains(fileName))
+                {
+                    links.Add(item);
+                }
+            }
+
+            if (links.Count > 0)
+            {
+                Response.Redirect(Request.RawUrl);
+            }
+            else
+                showPopup("No files could be retrieved.");
+        }
+
+        catch(Exception ex)
+        {
+            Debug.WriteLine("Exception ::" + ex.Message);
+        }
+    }
+
+    protected void downloadFile(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Test");
+        //Button btn = (Button)sender;
+        //Debug.WriteLine("ID ::" + btn.ID);
+    }
+
+
+    private void showPopup(String text)
+    {
+        Response.Write("<script>alert(' " + text + "');</script>");
+    }
+}
